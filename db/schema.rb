683c9398a3b5b6fd86bd0d99a085c3340caf3018
ANYTIME APPLIANCE <anytimeappliance@ANYTIMEs-Mac-mini.local>
2021-07-22 14:10:47 -0400

    #7 migrated messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_22_175337) do
+ActiveRecord::Schema.define(version: 2021_07_22_175655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.string "context"
+    t.bigint "user_id", null: false
+    t.bigint "room_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -28,4 +38,6 @@ ActiveRecord::Schema.define(version: 2021_07_22_175337) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "users"
 end
