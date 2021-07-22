53d95aaac9bc5ce3a24088541afcf4855c50647d
ANYTIME APPLIANCE <anytimeappliance@ANYTIMEs-Mac-mini.local>
2021-07-22 13:29:43 -0400

    #2 db:migrate and db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+@random_user = User.create!(username: 'gtalon00', email: 'email@email.com', password: '123456')
+
+puts "#{User.count} user(s) created"
