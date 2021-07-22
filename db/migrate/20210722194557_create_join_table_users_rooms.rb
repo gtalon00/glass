2b371329622f00ba91f9318816479cb604a5ab61
ANYTIME APPLIANCE <anytimeappliance@ANYTIMEs-Mac-mini.local>
2021-07-22 16:16:21 -0400

    #9 join table for users and rooms migration deleted. Added column to users. Not migrated.

--- db/migrate/20210722194557_create_join_table_users_rooms.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableUsersRooms < ActiveRecord::Migration[6.1]
-  def change
-    create_join_table :users, :rooms do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :room, null: false, foreign_key: true
-    end
-  end
-end
