d8b3605de9605d274321a325af40eaf56874b5c2
ANYTIME APPLIANCE <anytimeappliance@ANYTIMEs-Mac-mini.local>
2021-07-23 12:10:35 -0400

    #15 completed services files

--- client/src/services/rooms.js
+++ client/src/services/rooms.js
@@ -5,12 +5,12 @@ export const getAllRooms = async () => {
   return resp.data
 }
 
-export const get = async () => {
-  const resp = await api.(`/`)
+export const postRoom = async (id, userData) => {
+  const resp = await api.post(`/rooms/${id}`, userData)
   return resp.data
 }
 
-export const get = async () => {
-  const resp = await api.(`/`)
+export const deleteRoom = async (id) => {
+  const resp = await api.delete(`/rooms/${id}`)
   return resp.data
 }
