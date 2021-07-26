c0615d07f7d458c6be755f65331b4132d8960404
ANYTIME APPLIANCE <anytimeappliance@ANYTIMEs-Mac-mini.local>
2021-07-26 09:18:41 -0400

    #24 centered user img on pop up nav, starting to create the links.

--- client/src/components/BottomSlideNav.jsx
+++ client/src/components/BottomSlideNav.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import '../assets/CSS/BottomSlideNav.css'
 
 export default function BottomSlideNav({currentUser}) {
@@ -6,11 +7,13 @@ export default function BottomSlideNav({currentUser}) {
   return (
     <div className='bsn-parent'>
       <div className='bsn-top'>
-        <div>
-        <img src={currentUser.profile_pic} />
+        <div className='bsn-top-img-container'>
+        <img className='bsn-top-img' src={currentUser.profile_pic} />
         </div>
       </div>
-      <div className='bsn-bottom'></div>
+      <div className='bsn-bottom'>
+        <Link to=''></Link>
+      </div>
     </div>
   )
 }
