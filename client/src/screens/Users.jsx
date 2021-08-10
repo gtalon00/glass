902531e9f16b78ab3ff9543c4f15f2bf25dfd8f3
ANYTIME APPLIANCE <anytimeappliance@ANYTIMEs-Mac-mini.local>
2021-08-10 12:17:08 -0400

    #93 Tested and like template for tablet media queries for tablet.

--- client/src/screens/Users.jsx
+++ client/src/screens/Users.jsx
@@ -11,35 +11,36 @@ export default function Users({
 }) {
   return (
     <div className="users-parent">
-      <div className="users-glass-card-nav-container"></div>
-      <LeftSideNav currentUser={currentUser} setCurrentUser={setCurrentUser} />
       <div className="users-glass-card">
-        <div className="users-contact-header-container">
-          <h3>Contact Someone New:</h3>
-        </div>
-        <div className="users-list">
-          {listOfUsers &&
-            listOfUsers.map((user) => (
-              <div
-                className="users-profile-card"
-                onClick={() => handleCreateRoom(user.id)}
-                key={user.id}
-              >
-                <div className="users-profile-card-top">
-                  <img
-                    className="users-profile-img"
-                    src={
-                      user.profile_pic
-                        ? user.profile_pic
-                        : "https://i.imgur.com/rZOVQuh.jpg"
-                    }
-                    alt="Profle"
-                  />
+        <div className="user-glass-card-lft"></div>
+        <div className="user-glass-card-rgt">
+          <div className="users-contact-header-container">
+            <h3>Contact Someone New:</h3>
+          </div>
+          <div className="users-list">
+            {listOfUsers &&
+              listOfUsers.map((user) => (
+                <div
+                  className="users-profile-card"
+                  onClick={() => handleCreateRoom(user.id)}
+                  key={user.id}
+                >
+                  <div className="users-profile-card-top">
+                    <img
+                      className="users-profile-img"
+                      src={
+                        user.profile_pic
+                          ? user.profile_pic
+                          : "https://i.imgur.com/rZOVQuh.jpg"
+                      }
+                      alt="Profle"
+                    />
 
-                  <p className="users-profile-name">{user.username}</p>
+                    <p className="users-profile-name">{user.username}</p>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+          </div>
         </div>
       </div>
       <div className="bsn-placholder"></div>
